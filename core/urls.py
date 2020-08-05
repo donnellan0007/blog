b4b2c5d0788ddcd904777fc706180eeca7bfd46d
Seamus Donnellan <donnellan0007@gmail.com>
2020-08-05 16:40:31 +1000

    Initial Commit

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, register, verify, validate_username
+from .views import index
 from . import views
 
 app_name = "core"
 
 urlpatterns = [
     path('',views.index,name="index"),
-    path('register',views.register,name="register"),
-    path('verify', views.verify, name='verify'),
-    path('ajax/validate_username/',views.validate_username,name='validate_username'),
 ]
