ead85ff3004c58f0834d974c534b04337c5428a7
Seamus Donnellan <donnellan0007@gmail.com>
2020-09-02 01:51:24 +1000

    PWA Included

--- staticfiles/js/app.js
+++ staticfiles/js/app.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 var i = "Hello World";
 console.log(i)
-=======
 feather.replace();
 
 var profile = document.getElementById('profile-nav-stuff');
@@ -17,4 +15,3 @@ $("#osc-nav-he").click(function() {
 $("#id_username").change(function() {
     console.log($(this).val());
 })
->>>>>>> 1abcae6c3746e6722ee8c855cb397d7f3f6bc99a
