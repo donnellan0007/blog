02c8850688422e3b685ffac10c32bf3e7a7c2e7a
Seamus Donnellan <donnellan0007@gmail.com>
2020-09-11 08:28:15 +1000

    Postgres added

--- magazine/settings.py
+++ magazine/settings.py
@@ -84,27 +84,27 @@ WSGI_APPLICATION = 'magazine.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blogdb',
-        'USER': 'donnellan0007',
-        'PASSWORD': 'Stevemum1',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
-import psycopg2
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'blogdb',
+#         'USER': 'donnellan0007',
+#         'PASSWORD': 'Stevemum1',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
